52a6eedb7eedf34b6e240817ab0ca31db0c582b1
Nick Green <nick@nick8green.co.uk>
2025-03-17 14:10:51 +0000

    feat: added a QR code component

---
+++ src/components/QRCode/QRCode.stories.tsx
@@ -0,0 +1,57 @@
+import { type ComponentProps } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import QRCode, { type QRCodeDisplayOptions } from "components/QRCode";
+
+type QRCodeStoryProps = ComponentProps<typeof QRCode> & QRCodeDisplayOptions & { imagePath: string };
+
+const meta: Meta<QRCodeStoryProps> = {
+  title: "Display/QRCode",
+  component: QRCode,
+  argTypes: {
+    background: {
+        control: {
+            type: "color",
+        },
+        description: "Part of the display properties. The background color of the QR code.",
+    },
+    foreground: {
+        control: {
+            type: "color",
+        },
+        description: "Part of the display properties. The foreground color of the QR code.",
+    },
+    imagePath: {
+        control: {
+            type: "text",
+        },
+        description: "Part of the display properties. The path to the image to overlay on the QR code.",
+    },
+    display: {
+      control: false,
+    },
+    title: {
+      control: {
+        type: "text",
+      },
+    },
+    value: {
+      control: {
+        type: "text",
+    },
+  },
+  },
+  render: ({ background, foreground, imagePath, ...props }) => {
+    return (<QRCode {...props} display={{ background, foreground, image: imagePath }} />)
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof QRCode>;
+
+export const Default: Story = {
+  args: {
+    title: "Example QR Code",
+    value: "https://example.com",
+  },
+};
