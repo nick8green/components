dabb957f0d8e913836aa3f75dab94b05ed38a4b0
Nick Green <nick@nick8green.co.uk>
2025-09-04 16:08:43 +0100

    ci: updated change log and versioning config

---
+++ release.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  branches: [
+    "release",
+    {
+      channel: "canary",
+      name: "^(?!develop|main|release).*$",
+      prerelease: "${name}.rc",
+    },
+  ],
+  ci: true,
+  debug: true,
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "./plugins/conditional-changelog.js",
+      {
+        changelogFile: "CHANGELOG.md",
+        assets: ["package.json", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/npm",
+  ],
+};
