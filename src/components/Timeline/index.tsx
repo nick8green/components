32acb4c91d15dda5dd88ecfb0afdf3f97c268fd8
Nick Green <nick@nick8green.co.uk>
2025-10-19 23:28:11 +0100

    fix(Timeline): reworked styling for horizontal timelines

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -4,18 +4,17 @@
  *   - https://medium.com/free-code-camp/how-to-create-a-timeline-component-with-react-1b216f23d3d4
  */
 import type { FC } from "react";
-
-import "./style.css";
 import {
   Item as ItemComponent,
   type ItemProps as Item,
 } from "components/Timeline/Item";
 
+import "./style.css";
+
 export type TimelineProps = {
   dateFormat?: string;
   dateLocation?: "default" | "alternate";
-  display?: "vertical";
-  // display?: "horizontal" | "vertical";
+  display?: "horizontal" | "vertical";
   order?: "asc" | "desc";
   items: Item[];
 };
@@ -29,20 +28,22 @@ export const Timeline: FC<TimelineProps> = ({
 }) => {
   return (
     <div className={`timeline ${display}`}>
-      {items
-        .toSorted((e1: Item, e2: Item) =>
-          order === "desc"
-            ? e2.date.getTime() - e1.date.getTime()
-            : e1.date.getTime() - e2.date.getTime(),
-        )
-        .map((item: Item) => (
-          <ItemComponent
-            key={item.date.getTime()}
-            display={dateLocation}
-            format={dateFormat}
-            {...item}
-          />
-        ))}
+      <div className="timeline-track">
+        {items
+          .toSorted((e1: Item, e2: Item) =>
+            order === "desc"
+              ? e2.date.getTime() - e1.date.getTime()
+              : e1.date.getTime() - e2.date.getTime(),
+          )
+          .map((item: Item) => (
+            <ItemComponent
+              key={item.date.getTime()}
+              display={dateLocation}
+              format={dateFormat}
+              {...item}
+            />
+          ))}
+      </div>
     </div>
   );
 };
